defa7cacd9cc3689ddad40b13117f0ac004ea6aa
Thays Fernandes <thaysfernandes.ti@gmail.com>
2022-06-21 20:17:40 -0300

    Ajustes

--- open_cam.py
+++ open_cam.py
@@ -1,10 +1,11 @@
 import cv2 as cv
 import functions
-import os
 
 cam = cv.VideoCapture(0) #Iniciando a WebCam
 file_name = "haarcascade_frontalface_alt2.xml"
-classifier = cv.CascadeClassifier(f"{cv.haarcascades}{os.sep}{file_name}") #Modelo para reconhecer faces
+face_cascade = cv.CascadeClassifier('/Users/thaysss/Documents/AuladeIA/opencv/data/haarcascades_cuda/haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier('/Users/thaysss/Documents/AuladeIA/opencv/data/haarcascades/haarcascade_eye.xml')
+
 
 dataframe = functions.load_dataframe() #Carregando dataframe com as imagens para treinamento
 
@@ -35,30 +36,41 @@ while True:
     #Transformando a imagem em escala de cinza
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     
-    #Detectando faces na imagem
-    faces = classifier.detectMultiScale(gray)
+#Detectando faces na imagem
+faceCascade = cv.CascadeClassifier('/Users/thaysss/Documents/AuladeIA/opencv/data/haarcascades_cuda/haarcascade_frontalface_default.xml')
 
-    #Iterando nas faces encontradas
-    for x,y,w,h in faces:
-        gray_face = gray[y:y+h, x:x+w] #Recortand região da face
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+)
 
-        if gray_face.shape[0] >= 200 and gray_face.shape[1] >= 200:
-            gray_face = cv.resize(gray_face, (160,160)) #Redimensionando
-            vector = pca.transform([gray_face.flatten()]) #Extraindo features da imagem
+print(type(faces))
+print(faces)
 
-            pred = knn.predict(vector)[0] #Classificando a imagem
-            classification = label[pred]
 
-            #Desenhando retangulos em torno da face
-            if pred == 0:
-                cv.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 3)
-                print("\a")
-            elif pred == 1:
-                cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
+#Iterando nas faces encontradas
+for x,y,w,h in faces:
+ gray_face = gray[y:y+h, x:x+w] #Recortand região da face
+
+if gray_face.shape[0] >= 200 and gray_face.shape[1] >= 200:
+ gray_face = cv.resize(gray_face, (160,160)) #Redimensionando
+vector = pca.transform([gray_face.flatten()]) #Extraindo features da imagem
+
+pred = knn.predict(vector)[0] #Classificando a imagem
+classification = label[pred]
+
+#Desenhando retangulos em torno da face
+if pred == 0:
+    cv.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 3)
+    print("\a")
+elif pred == 1:
+    cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
             
-            #Escrevendo classificação e quantidade de faces vistas
-            cv.putText(frame, classification, (x - 20,y + h + 50), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2, cv.LINE_AA)
-            cv.putText(frame, f"{len(faces)} rostos identificados",(20,20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2, cv.LINE_AA)
+#Escrevendo classificação e quantidade de faces vistas
+cv.putText(frame, classification, (x - 20,y + h + 50), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2, cv.LINE_AA)
+cv.putText(frame, f"{len(faces)} rostos identificados",(20,20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2, cv.LINE_AA)
 
-    #Mostrando o frame
-    cv.imshow("Cam", frame)
+#Mostrando o frame
+cv.imshow("Cam", frame)
+
--- open_cam_no_face_detection.py
+++ open_cam_no_face_detection.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import functions
-import os
+
 
 cam = cv.VideoCapture(0) #Iniciando a WebCam
 
